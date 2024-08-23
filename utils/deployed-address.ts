2752fa0291c109a9fea635ee344974aafa179362
phamvan1607 <phamngoctuongvan1995@gmail.com>
2024-08-23 17:15:55 +0000

    upppasas

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xE6351233E22fceF7B594Dd43cC9e4a0Fe92856de'
+export const ProxyAdmin = '0x65Bd9F1bAc769aa6f84B2F2Fc5999607c46Ed9dd'
+export const SWTRImplementation = '0x87aE00a3F1EB4aEfD33a3Fd4825a2eD8A4F890bE'
     
